60926c81410d76e1b0ea0e0aa13e9865ba494dda
shame-game <hoanghung.vam1209@gmail.com>
2024-01-19 22:10:51 +0700

    d

---
+++ assets/js/load-content.js
@@ -0,0 +1,309 @@
+
+
+fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "header",
+  })
+  .then((rows) => {
+    let imageUrl = rows[0].imageUrl || "assets/images/header_app.png";
+    document.querySelector("#header-app").innerHTML = `
+          <div
+            class="image wow fadeInRightBig"
+            data-wow-duration="1.3s"
+            data-wow-delay="1.8s"
+          >
+            <img src="${imageUrl}" alt="header App" />
+            <img src="assets/images/dots.svg" alt="dots" class="dots" />
+          </div>
+`;
+  });
+
+fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "gallery",
+  })
+  .then((rows) => {
+    let galleryHtml = "";
+
+    rows.forEach((row) => {
+      galleryHtml += `
+        <div class="single_slider">
+          <img src="${row.imageUrl}" alt="Screen Shot" />
+        </div>
+      `;
+    });
+
+    document.querySelector("#gallery").innerHTML = galleryHtml;
+
+    $(".screenshot_active").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      pauseOnHover: false,
+      prevArrow: '<span class="prev"><i class="lni lni-arrow-left"></i></span>',
+      nextArrow:
+        '<span class="next"><i class="lni lni-arrow-right"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+
+fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "timeline",
+  })
+  .then((rows) => {
+    let timelineHtml = "";
+    rows.forEach((row) => {
+      timelineHtml += `
+      <div class="col-lg-4 col-md-8">
+      <div class="single_blog blog_1 mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 1.3s; animation-delay: 0.2s; animation-name: fadeInUp;">
+        <div class="blog_image">
+          <img src="${row.row3}" alt="blog">
+        </div>
+        <div class="blog_content">
+          <div class="blog_meta d-flex justify-content-between">
+            <div class="meta_date">
+              <span>Giá: ${row.row1}</span>
+            </div>
+            <div class="meta_like"></div>
+          </div>
+          <h4 class="blog_title">
+          <a>${row.row4}</a>
+          </h4>
+          <p style="display: -webkit-box;
+          -webkit-line-clamp: 5;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-align: justify">${row.row2}</p>
+        </div>
+      </div>
+    </div>`;
+    });
+
+    document.querySelector("#timeline").innerHTML = timelineHtml;
+    $("#timeline").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      pauseOnHover: false,
+      prevArrow: '<span class="prevv"><i class="bi bi-arrow-left-short"></i></span>',
+      nextArrow:
+        '<span class="nextt"><i class="bi bi-arrow-right-short"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+  fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "bienca",
+  })
+  .then((rows) => {
+    let timelineHtmle = "";
+    rows.forEach((row) => {
+      timelineHtmle += `
+      <div class="col-lg-4 col-md-8">
+      <div class="single_blog blog_1 mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 1.3s; animation-delay: 0.2s; animation-name: fadeInUp;">
+        <div class="blog_image">
+          <img src="${row.row3}" alt="blog">
+        </div>
+        <div class="blog_content">
+          <div class="blog_meta d-flex justify-content-between">
+            <div class="meta_date">
+              <span>Giá: ${row.row1}</span>
+            </div>
+            <div class="meta_like"></div>
+          </div>
+          <h4 class="blog_title">
+          <a>${row.row4}</a>
+          </h4>
+          <p style="display: -webkit-box;
+          -webkit-line-clamp: 5;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-align: justify">${row.row2}</p>
+        </div>
+      </div>
+    </div>`;
+    });
+
+    document.querySelector("#timeline1").innerHTML = timelineHtmle;
+    $("#timeline1").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      pauseOnHover: false,
+      prevArrow: '<span class="prevv"><i class="bi bi-arrow-left-short"></i></span>',
+      nextArrow:
+        '<span class="nextt"><i class="bi bi-arrow-right-short"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+  fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "luacho",
+  })
+  .then((rows) => {
+    let timelineHtmle = "";
+    rows.forEach((row) => {
+      timelineHtmle += `
+      <div class="col-lg-4 col-md-8">
+      <div class="single_blog blog_1 mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 1.3s; animation-delay: 0.2s; animation-name: fadeInUp;">
+        <div class="blog_image">
+          <img src="${row.row3}" alt="blog">
+        </div>
+        <div class="blog_content">
+          <div class="blog_meta d-flex justify-content-between">
+            <div class="meta_date">
+              <span>Giá: ${row.row1}</span>
+            </div>
+            <div class="meta_like"></div>
+          </div>
+          <h4 class="blog_title">
+          <a>${row.row4}</a>
+          </h4>
+          <p style="display: -webkit-box;
+          -webkit-line-clamp: 5;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-align: justify">${row.row2}</p>
+        </div>
+      </div>
+    </div>`;
+    });
+
+    document.querySelector("#timeline2").innerHTML = timelineHtmle;
+    $("#timeline2").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      pauseOnHover: false,
+      prevArrow: '<span class="prevv"><i class="bi bi-arrow-left-short"></i></span>',
+      nextArrow:
+        '<span class="nextt"><i class="bi bi-arrow-right-short"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+fetchSheet
+  .fetch({
+    gSheetId: "1Qf2R0ARI0U207hw4lm6qRvwEFkHFD3wy4wOVG45GXrk",
+    wSheetName: "video",
+  })
+  .then((rows) => {
+    document.querySelector("#video iframe").src = rows[0].videoUrl;
+  });
