c4bf736304eb608b081f79e28f0ee56c861fd746
shame-game <hoanghung.vam1209@gmail.com>
2024-01-20 13:48:48 +0700

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".header_navbar").removeClass("sticky");
-            $(".header_navbar img").attr("src", "");
+            $(".header_navbar img").attr("src", "assets/images/dsh.png");
         } else {
             $(".header_navbar").addClass("sticky");
-            $(".header_navbar img").attr("src", "");
+            $(".header_navbar img").attr("src", "assets/images/dsh.png");
         }
     });
     
